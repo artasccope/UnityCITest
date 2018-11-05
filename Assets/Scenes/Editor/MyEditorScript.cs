0d5211501852ebab9217fb8c575375899f631da3
MX <398117200@qq.com>
2018-11-05 13:36:19 +0800

    build test

---
+++ Assets/Scenes/Editor/MyEditorScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+using UnityEngine;
+ 
+public class MyEditorScript 
+{
+	public static void MyMethod ()
+	{
+			string executeMethod = "MyEditorScript.MyMethod";
+			foreach(string arg in System.Environment.GetCommandLineArgs()) {
+				Debug.Log(arg);
+			}
+	}
+}

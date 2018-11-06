a435bed76791d77d59f3b7cc63994c20780441a2
MX <398117200@qq.com>
2018-11-06 17:42:57 +0800

    test results

---
+++ Assets/Scenes/Editor/Tests/NewTestScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class NewTestScript {
+
+    [Test]
+    public void NewTestScriptSimplePasses() {
+        //Assert.Pass();
+    }
+    
+    [Test]
+    public void NewTestScriptSimplePasses2() {
+        Assert.Fail();
+    }
+
+}
